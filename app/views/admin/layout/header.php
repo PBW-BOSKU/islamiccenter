3be25f6fa921d94708aeba5fdecc2633c25a9e8b
sirrelll <darel2227@gmail.com>
2026-04-29 13:59:56 +0800

    logo

--- app/views/admin/layout/header.php
+++ app/views/admin/layout/header.php
@@ -15,6 +15,8 @@ if(!isset($_SESSION['admin'])){
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title>Admin Dashboard</title>
 
+<link rel="icon" type="image/png" href="/assets/images/logonewislamic.png">
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
